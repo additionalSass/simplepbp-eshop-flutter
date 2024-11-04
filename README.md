61af0d70e68f154864f8417543eb0bf8f1625e87
Vander Gerald <vander.gerald@ui.ac.id>
2024-11-04 14:31:45 +0700

    tugas 7

--- README.md
+++ README.md
@@ -1,16 +1,53 @@
 # simplepbp_eshop_flutter
 
-A new Flutter project.
+A new Flutter project. Tugas 7
 
-## Getting Started
+## Menjawab beberapa pertanyaan berikut pada README.md pada root_folder.
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+### 1. Jelaskan apa yang dimaksud dengan stateless widget dan stateful widget, dan jelaskan perbedaan dari keduanya.
+Stateless widget adalah widget yang immutable, propertinya tidak berubah saat mereka di-set. Widget ini digunakan untuk menampilkan konten statis yang itdak memerlukan update variable ketika runtime aplikasi.
+Stateful widget adalah widget yang mutable, penampilannya dapat berubah sebagai respons terhadap event yang di-trigger oleh interaksi user dengan aplikasi dengan menyimpan state mutable yang bisa di-update yang membuat widget mampu dibangun ulang dan merepresentasikan perubahan dinamis. Digunakan untuk komponen interaktif seperti button yang bisa on and off , counter, atau segala elemen yang memerlukan untuk menyimpan update dalam respons terhadap input user.
+Perbedaannya adalah yang pertama immutable, digunakan untuk konten statis yang tidak berubah, lebih efisien dalam performa karena tidak ada management of state dan yang terakhir mutable, digunakan untuk konten dinamis yang memerlukan update variable-variable sebagai respons interaksi user, lebih banyak overhead dalam performance.
+### Sebutkan widget apa saja yang kamu gunakan pada proyek ini dan jelaskan fungsinya.
+1. MyApp (StatelessWidget): Berperan sebagai root aplikasi, me-return sebuah widget MaterialApp.
+2. MaterialApp: Menentukan title aplikasi, tema, dan home screen (di sini Homepage()).
+3. Homepage (StatelessWidget): Layar utama app
+4. Scaffold: Memberikan struktur layout mendasar untuk Homepage, mencakup sebuah AppBar dan sebuah body.
+5. AppBar: Menampilkan title "Simple PBP E-Commerce Homepage" di bagian atas halaman
+6. body: Padding: Menambah padding untuk child,  Row, supaya konten tidak menyentuh ujung 
+7. Row: Untuk menyusun tiga widget ElevatedButton.icon yang dipisahkan oleh widget SizedBox.
+8. ElevatedButton.icon: ButtonStyle bisa ditiru dari satu final buttonStyle variable yang ada di awal kode build() . Setiap buton memiliki icon dan label dan background color yang berbeda. Setiap kali diklik, akan muncul SnackBar karena _showSnackBar(context, message) dipanggil.
+- Button 1:
+ Icon: Icons.view_list
+ Label: 'Lihat Daftar Produk'
+ Color: Colors.lightBlueAccent
+-  Button 2:
+ Icon: Icons.add_shopping_cart
+ Label: 'Tambah Produk'
+ Color: Colors.lightGreenAccent
+-  Button 3:
+ Icon: Icons.logout
+ Label: 'Logout'
+ Background Color: Colors.redAccent
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+9. SizedBox: Memaksikan button memiliki jarak
+10. SnackBar: Menampilkan pesan singkat di bagian bawah layar ketika tombol ditekan.
+11. ScaffoldMessenger: Mengatur penampilan Snackbar. Memastikan bahwa SnackBar dihilangkan dulu sebelum menunjukkan yang baru.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### Apa fungsi dari setState()? Jelaskan variabel apa saja yang dapat terdampak dengan fungsi tersebut.
+Fungsinya adalah membangun ulang user interface untuk merefleksikan perubahan-perubahan data atau state yang dienkapsulasi di dalam setState() . State variabel dan collection seperti List, Map bisa memperoleh dampak dengan fungsi tersebut. Misalnya, bool _isVisible = true , int counter = 1 , List<String> _items . Perhatikan bahwa "_" di sini berkaitan dengan privacy dari variable method terhadap librarynya. Hal ini tidak dibutuhkan dalam setState(), tetapi biasanya digunakan untuk privacy . 
+### Jelaskan perbedaan antara const dengan final 
+Keyword const berarti variabel itu konstan saat compile-time , variabelnya wajib diketahui saat compile. Keyword final dinisialisasi sekali saja tetapi dievaluasi saat runtime, bukan compiletime, sehingga nilai bisa ditentukan saat kode dijalankan sehingga final lebih fleksibel dibandingkan const. Tetapi tentu saja final tidak bisa berubah begitu di-assign nilai tertentu.
+### Jelaskan bagaimana cara kamu mengimplementasikan checklist-checklist di atas.
+1. Membaca dan memahami Tugas dan hal-hal yang harus dibuat.
+2. Menentukan rencana layout menggunakan Row.
+3. Membuat project Flutter dengan flutter create
+4. Membuat Scaffold dengan AppBar
+5. Memikirkan dan menulis kode untuk button dan style
+6. Membuat _showSnackbar
+7. Membuat interaksi button jika onPressed dengan _showSnackbar
+8. Menambahkan padding dan sizedbox dan menulis kode widget
+9. Mencoba dengan flutter run 
+10. Mengubah warna dan ukuran-ukuran tertentu 
+11. Mengecek bahwa sesuai dengan tugas
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simple PBP E-Commerce App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Homepage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class Homepage extends StatelessWidget {
+  void _showSnackbar(BuildContext context, String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 4),
+    );
+    var currMessenger = ScaffoldMessenger.of(context);
+    currMessenger.removeCurrentSnackBar();
+    currMessenger.showSnackBar(snackBar);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      minimumSize: Size(40, 40),
+      textStyle: TextStyle(fontSize: 16),
+    );
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Simple PBP E-Commerce Homepage'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Button 1 untuk Lihat Daftar Produk
+            ElevatedButton.icon(
+              style: buttonStyle.copyWith(
+                backgroundColor:
+                    WidgetStateProperty.all<Color>(Colors.lightBlueAccent),
+              ),
+              icon: Icon(Icons.view_list),
+              label: Text('Lihat Daftar Produk'),
+              onPressed: () {
+                _showSnackbar(
+                    context, 'Kamu telah menekan tombol Lihat Daftar Produk');
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(width: 25), // Spacing dari kedua button
+
+            // Button 2 untuk Tambah Produk
+            ElevatedButton.icon(
+              style: buttonStyle.copyWith(
+                backgroundColor:
+                    WidgetStateProperty.all<Color>(Colors.lightGreenAccent),
+              ),
+              icon: Icon(Icons.add_shopping_cart),
+              label: Text('Tambah Produk'),
+              onPressed: () {
+                _showSnackbar(
+                    context, 'Kamu telah menekan tombol Tambah Produk');
+              },
+            ),
+            SizedBox(width: 25),
+
+            // Button 3 untuk Logout
+            ElevatedButton.icon(
+              style: buttonStyle.copyWith(
+                backgroundColor:
+                    WidgetStateProperty.all<Color>(Colors.redAccent),
+              ),
+              icon: Icon(Icons.logout),
+              label: Text('Logout'),
+              onPressed: () {
+                _showSnackbar(context, 'Kamu telah menekan tombol Logout');
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
